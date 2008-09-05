d4310692d3f5686e95e4e14556bd6af536c075fc
Dai Coden <daicoden@copypastel.com>
2008-09-05 19:29:00 -0400

    First Attempt is almost donegit add restful_additions.rb  Found a different way to edit the associations so it is working much better now.  Right now you have to use the id of a foreign key.  Soon that will be changed to use the restful_keys of the foreign key as well.  in other words members/daicoden/posts does not work right now... but members/1/posts if daicoden is the first record will return all the posts of daicoden with only writing this code...  @posts = request.records or @posts = request.records_for_page(params[:page]) or @post = request.records_for_page.

--- restful_additions.rb
+++ restful_additions.rb
@@ -21,63 +21,63 @@ module RestfulAdditions
     # * For instance if a post belongs_to a topic then visiting http://blog.com/topic/1/posts would yeild
     # params to contain topic_id => 1 which can then be used to find all the posts for that topic
     def restful_find(mode, params = { },conditions = { })
-      rep = []
+      debug = []
       for association in @restful_associations
         conditions.merge!(association.in(params)) if(association.is_in?(params))
-        rep.push(association.in(params).to_s)
+        debug.push(association.in(params))
       end
 
-      raise rep.to_s
       self.restful_find_by_keys(mode,params,conditions)
     end
-    
-    def add_restful_association(macro,name,options,class_name)
-      case(macro)
-      when :belongs_to
-        raise name + " " + class_name
-#          raise options if name == "fable"
-          @restful_associations.push(RestfulAssociation.new(name,options[:class_name] || class_name))
-      when :has_many:
-          @restful_associations.push(RestfulAssociation.new(name,options[:class_name] || class_name))
-      else
-        puts "Warning... #{macro} not supported yet by restful additions"
+
+    def restful_paginate(params,args = { })
+      args[:conditions] ||= { }
+      for association in @restful_associations
+        args[:conditions].merge!(association.in(params)) if(association.is_in?(params))
       end
-      
+
+      self.paginate(args)
+    end
+    
+    def add_restful_association(name,target,options)
+      @restful_associations.push(RestfulAssociation.new(name.to_s, target.to_s,options))     
     end
 
     
     protected
     def restful_find_by_keys(mode,params,conditions)
-      @restful_keys.each do |key|
-        item = self.send("find_by_#{key}",params["id"],conditions)
-        return item unless item.nil? 
+
+      if mode == :all
+        return self.find(:all,:conditions => conditions)
+      elsif mode == :first
+
+        @restful_keys.each do |key|
+          item = self.send("find_by_#{key}",params["id"],:conditions => conditions)
+          return item unless item.nil? 
+        end
+        
       end
       return nil
-    end
 
-    def restful_find_by_conditions(mode,params,conditions)
     end
     
-    def restful_find_by_subset(mode,params,conditions)    
-    end
-
     # To fully understand the class read it like:
     # Class_name method paramsters
     # For isntance initialize is read like this
     # RestfulAssociation initialize link name to class
     class RestfulAssociation
       ## To class not used temporarily since belongs_to is only one currently
-      def initialize(owner_class,child_class)
-        raise owner_class if( child_class == "Member")
+      def initialize(owner_class,child_class,options)
+        @link_name_to_class_name = { child_class.downcase+"_id" => options[:class_name].downcase+"_id" }
         @link_name = child_class.downcase + "_id"
       end
       
       def in(params)
-        { @link_name => params[@link_name] }
+        { @link_name => params[@link_name] || params[@link_name_to_class_name[@link_name]] }
       end
       
       def is_in?(params)
-        params.any? { |link,value| link == @link_name}
+        params.any? { |link,value| link == @link_name or link == @link_name_to_class_name[@link_name]}
       end
     end
   end
@@ -88,19 +88,16 @@ module ActiveRecord
     extend RestfulAdditions::ModelBase
   end
 
-  module Reflection # :nodoc:
+  module Associations
     module ClassMethods
-      alias old_create_reflection create_reflection
-      def create_reflection(macro,name,options,active_record)
-        reflec = old_create_reflection(macro,name,options,active_record)
-        begin
-          reflec.klass.add_restful_association(macro,reflec.klass.to_s,options,name.to_s)
-        rescue NameError
-        end
-        reflec
+      alias old_create_belongs_to_reflection create_belongs_to_reflection
+      def create_belongs_to_reflection(association_id, options)
+        self.add_restful_association(self.name,association_id,options)
+        old_create_belongs_to_reflection(association_id,options)
       end
     end
   end
+
 end
  
 
@@ -126,7 +123,7 @@ class ActionController::AbstractRequest
     args.merge!(:page => page)
 
     class_const = extract_record_class()
-    class_const.paginate(args)
+    class_const.restful_paginate(self.parameters,args)
   end
   
   def record
@@ -146,7 +143,7 @@ private
   #Returns single
   def extract_records(mode = :first, conditions = { })
     class_const = extract_record_class()
-    class_const.restful_find(mode, self.parameters,:conditions => conditions)
+    class_const.restful_find(mode, self.parameters,conditions)
   end
   
   def extract_record_class
